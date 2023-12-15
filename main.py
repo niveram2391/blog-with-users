6e0412f4ba684f5ee6e7629ba913273be02426d1
nramadurai <129441558+nramadurai@users.noreply.github.com>
2023-12-15 09:43:35 -0500

    Use env variables and created procfile

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 # Import your forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm,CommentForm
+import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -124,7 +126,7 @@ def login():
     if form.validate_on_submit():
         email = form.email.data
         password = form.password.data
-        uscder_details = db.session.execute(db.select(User).where(User.email== email))
+        user_details = db.session.execute(db.select(User).where(User.email== email))
         user = user_details.scalar()
         if user and check_password_hash(user.password, password):
             login_user(user)
@@ -225,4 +227,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False, port=5002)
